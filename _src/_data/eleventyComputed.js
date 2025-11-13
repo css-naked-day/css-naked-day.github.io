b12b38def32e2b5e98f50999fc02d52390344e0e
Fabien Basmaison <contact@arkhi.org>
2025-11-13 23:07:58 +0100

    feat(#160): Flatten data structure:
    * Use array-like data to define what to get from the global data. This allows nunjucks filters.
    * Add globals for first and current year.
    * Add filters and clean config file a bit.

--- _src/_data/eleventyComputed.js
+++ _src/_data/eleventyComputed.js
@@ -1,50 +1,34 @@
 export default {
-	// Provide a structure by year to match expected output logic.
-	editions: function(data) {
+	// participations: { year, participant }[]
+	participations: function(data) {
 		const participants = data.participants;
-		const editions = [];
-		const yearMin = 2006; // The first CSS Naked Day.
-
-		let yearMax = yearMin;
+		const participations = [];
 
 		for (const participant in participants) {
-			// Basic structure: year > participant > websites.
 			participants[participant].websites.forEach(website => {
 				website.years.forEach(year => {
-					let currentEdition = editions.find(edition => edition.year === year);
-
-					yearMax = Math.max(yearMin, year);
-
-					// Initialise year if it does not exist.
-					if (!currentEdition) {
-						editions.push({
-							year: Number(year),
-							'participants': {},
-						});
-
-						currentEdition = editions.find(edition => edition.year === year);
-					}
-
-					// Populate participants for this year.
-					if (!currentEdition['participants'][participant]) {
-						currentEdition['participants'][participant] = [];
-					}
-
-					// Populate participant with its websites for this year.
-					currentEdition['participants'][participant].push(website.url);
+					participations.push({year, participant});
 				});
 			});
 		};
 
-		// Add missing editions with no participant.
-		for (let year = yearMin; year < yearMax; year++) {
-			if (!editions.some((edition) => edition.year === year)) {
-				editions.push({ year });
-			}
-		}
+		participations.sort((a, b) => a.year > b.year ? 1 : -1);
 
-		editions.sort((a, b) => a.year > b.year ? 1 : -1);
+		return participations;
+	},
 
-		return editions;
-	}
+	// participationsCount: { YEAR: COUNT, … }
+	participationsCount: function(data) {
+		const participationsCount = {};
+
+		for(const participation of data.participations) {
+			const yearCount = participationsCount[participation.year];
+
+			participationsCount[participation.year] = yearCount
+				? participationsCount[participation.year] + 1
+				: 1;
+		};
+
+		return participationsCount;
+	},
 }
--- eleventy.config.js
+++ eleventy.config.js
@@ -7,6 +7,9 @@ function getWebsiteDomain(url) {
 	);
 }
 
+// TODO: Add a tool to target duplicated domains.
+// TODO: Add finer sorting.
+
 export default function (eleventyConfig) {
 	eleventyConfig.setQuietMode(true);
 
@@ -18,31 +21,50 @@ export default function (eleventyConfig) {
 	// Allow to parse Toml files for Global data.
 	eleventyConfig.addDataExtension('toml', (contents) => toml.parse(contents));
 
+	// Filters
+	// ===============================================================================================
+
+	// Global data filters.
+	// ---------------------------------------------------------------------------
+
 	eleventyConfig.addFilter('getParticipantDisplayName', (participant) => {
 		// TODO: Either get current year’s website, or use the filename.
 		const websiteURL = participant.websites[0].url;
 
-		return participant.display || participant.username || getWebsiteDomain(websiteURL);
+		return participant.display ?? participant.username ?? getWebsiteDomain(websiteURL);
 	});
 
 	eleventyConfig.addFilter('getSiteTitle', (url, participant) => {
 		const website = participant.websites.find(website => website.url === url);
 
-		return website.title || getWebsiteDomain(website.url);
+		return website.title ?? getWebsiteDomain(website.url);
 	});
 
 	// Return website matching url and year.
 	// This allows to have different configurations for different years.
 	// This is uSeful for prefix and suffix which might need different values depending on the year.
 	eleventyConfig.addFilter('getSiteData', (url, participant, year) => {
-		return participant.websites.find(website => {
-			return website.url === url && website.years.includes(year);
-		});
+		return participant.websites.find(website => website.url === url);
 	});
 
-	eleventyConfig.addShortcode('linkNoSpam', function(callback, url, participant, year, loopRevIndex0) {
+	eleventyConfig.addFilter('getWebsitesForYear', (websites, year) => {
+		return websites.filter(website => website.years.includes(year));
+	});
+
+	// eleventyComputed data filters.
+	// ---------------------------------------------------------------------------
+
+	eleventyConfig.addFilter('getParticipantsForYear', (participations, year) => {
+		return participations
+			.filter(participation => participation.year === year)
+			.sort((a, b) => a.participant > b.participant ? 1 : -1);
+	});
+
+	// Shortcodes
+	// ===============================================================================================
+
+	eleventyConfig.addShortcode('linkNoSpam', function(callback, url, participant, year, loopIndex0) {
 		const website = eleventyConfig.getFilter('getSiteData')(url, participant, year);
-		const isWebsiteTitle = callback === 'getSiteTitle';
 
 		if(!website) {
 			return;
@@ -50,7 +72,7 @@ export default function (eleventyConfig) {
 
 		let title, prefix, suffix, separator;
 
-		if (isWebsiteTitle) {
+		if (callback === 'getSiteTitle') {
 			title  = eleventyConfig.getFilter('getSiteTitle')(url, participant);
 			prefix = website.prefix;
 			suffix = website.suffix;
@@ -58,8 +80,8 @@ export default function (eleventyConfig) {
 			title = eleventyConfig.getFilter('getParticipantDisplayName')(participant);
 		}
 
-		if (website.separator === undefined && loopRevIndex0) {
-			separator = loopRevIndex0 > 1 ? ' & ' : ', ';
+		if (website.separator === undefined && loopIndex0) {
+			separator = loopIndex0 > 1 ? ' & ' : ', ';
 		}
 
 		if (!website.url) {
@@ -72,8 +94,6 @@ export default function (eleventyConfig) {
 
 		return `${separator ?? ''}${prefix ?? ''}<a href="${website.url}">${title}</a>${suffix ?? ''}`
 	});
-
-	// TODO: Add a tool to target duplicated domains.
 
 	return {
 		htmlTemplateEngine: 'njk',
