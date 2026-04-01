8e71741f24242df1d6880f1f4b4b0b4ef158f8a7
Jens Oliver Meiert <jens@meiert.com>
2026-04-01 10:30:51 +0200

    chore: refactor variables
    Signed-off-by: Jens Oliver Meiert <jens@meiert.com>

--- _src/_data/globals.js
+++ _src/_data/globals.js
@@ -1,9 +1,10 @@
 const now = new Date();
-const year = now.getUTCFullYear();
+const yearCurrent = now.getUTCFullYear();
+const cssndStart = new Date(Date.UTC(yearCurrent, 3, 8, 10, 0, 0));
+const cssndEnd = new Date(Date.UTC(yearCurrent, 3, 10, 12, 0, 0));
 
 export default {
 	yearStart: 2006,
-	yearCurrent: year,
-	// “true” while April 9 is occurring anywhere on Earth (UTC+14 to UTC−12: April 8 10:00 UTC–April 10 12:00 UTC)
-	isNakedDay: now >= new Date(Date.UTC(year, 3, 8, 10, 0, 0)) && now < new Date(Date.UTC(year, 3, 10, 12, 0, 0)),
+	yearCurrent,
+	isNakedDay: now >= cssndStart && now < cssndEnd,
 }
